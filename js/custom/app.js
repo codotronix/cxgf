ecffa16e670b7e8c39321473ed9e5758317b2809
Suman Barick <codotronix@gmail.com>
2015-08-21 23:01:26 +0530

    background changed

--- js/custom/app.js
+++ js/custom/app.js
@@ -4,4 +4,12 @@ app.controller("mainCtrl", ['$scope','servc_cxge', function ($scope, cxge) {
 	$scope.field = {};
 	$scope.field.width = cxge.getScreenWidth();
 	$scope.field.height = cxge.getScreenHeight();
+
+	/*
+	function resolveCollision () {
+
+	}
+
+	cxge.onCollision(resolveCollision);
+	*/
 }]);
