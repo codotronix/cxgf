440071acb3a8b7b7a7a735f18236a70e93ae4a4d
Suman Barick <barick9@gmail.com>
2017-12-06 13:49:08 +0530

    website manual page started

--- www/js/manualController.js
+++ www/js/manualController.js
@@ -6,5 +6,26 @@
 
     function manualController ($rootScope) {
         $rootScope.pageID = 'manualpage';
+
+        var vm = this;
+
+        vm.topics = [
+            {
+                title: "GameObject Module (cxgf.GameObject)",
+                URI: "" 
+            },
+            {
+                title: "KeyEvent Module (cxgf.KeyEvent)",
+                URI: "" 
+            },
+            {
+                title: "Ticker Module (cxgf.Ticker)",
+                URI: "" 
+            },
+            {
+                title: "Collision Module (cxgf.Collision)",
+                URI: "" 
+            },
+        ];
     }
 })();
